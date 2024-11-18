d2c130c731d5b5882bb6c3ee652a27b4e9642238
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-17 20:57:06 -0600

    fix keymap

--- keyboards/crkbd/keymaps/onnenon/keymap.c
+++ keyboards/crkbd/keymaps/onnenon/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [SYM] = LAYOUT_split_3x5_3(
     KC_EXLM,           KC_AT,             KC_HASH,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_AMPR,           KC_ASTR,           KC_GRAVE,          KC_TILD,
-    KC_BSLS,           KC_PIPE,           KC_LPRN,           KC_RPRN,           KC_MINS,           KC_EQL,            KC_LSFT,           KC_LCTL,           KC_LALT,           LC_LGUI,
+    KC_BSLS,           KC_PIPE,           KC_LPRN,           KC_RPRN,           KC_MINS,           KC_EQL,            KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,
     KC_LT,             KC_GT,             KC_LCBR,           KC_RCBR,           KC_UNDS,           KC_PLUS,           KC_LBRC,           KC_RBRC,           U_NA,              U_NA,
                                           U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA
 ),
