39944d10db14ce225d316195ef8b669ea2741ccb
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-27 00:17:37 -0600

    ferris

---
+++ build.sh
@@ -0,0 +1,4 @@
+qmk compile -kb crkbd -km onnenon -e CONVERT_TO=elite_pi
+qmk compile -kb crkbd -km onnenon -e CONVERT_TO=blok
+qmk compile -kb boardsource/microdox/v1 -km onnenon
+qmk compile -kb ferris/0_1 -km onnenon
